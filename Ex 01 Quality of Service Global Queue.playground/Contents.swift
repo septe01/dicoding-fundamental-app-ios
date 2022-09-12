bd152f2aec072a35b4a70145216a371efce1383e
septe habudin <septe.vhirgo@gmail.com>
2022-09-12 21:10:43 +0700

    fix excercise QoS(Quality of Service) Global Queue

---
+++ Ex 01 Quality of Service Global Queue.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+enum Color: String{
+    case red = "red"
+    case blue = "blue"
+    case green = "green"
+}
+
+let count = 5
+
+func show(color: Color, count: Int) {
+    for _ in 1...count {
+        print(color.rawValue)
+    }
+}
+
+//initialisasi 3 intance secara berurutan dengan QoS
+let userInteractiveQueue = DispatchQueue.global(qos: .userInteractive)
+let utilityQueue = DispatchQueue.global(qos: .utility)
+let backgroundQueue = DispatchQueue.global(qos: .background)
+
+
+DispatchQueue.main.async {
+    utilityQueue.async {
+        show(color: .green, count: count)
+    }
+    
+    backgroundQueue.async {
+        show(color: .blue, count: count)
+    }
+    
+    userInteractiveQueue.async {
+        show(color: .red, count: count)
+    }
+}
+
+
