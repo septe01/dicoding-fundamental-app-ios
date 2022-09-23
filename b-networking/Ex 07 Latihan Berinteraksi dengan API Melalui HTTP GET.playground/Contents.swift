1a1995a2b96b70a3e897beff16a4b74072a62e0a
septe habudin <septe.vhirgo@gmail.com>
2022-09-23 21:47:06 +0700

    fix maping object

--- b-networking/Ex 07 Latihan Berinteraksi dengan API Melalui HTTP GET.playground/Contents.swift
+++ b-networking/Ex 07 Latihan Berinteraksi dengan API Melalui HTTP GET.playground/Contents.swift
@@ -24,6 +24,7 @@ let task = URLSession.shared.dataTask(with: request) { data, res, error in
     if let data = data {
         if response.statusCode == 200 {
             print("data \(data)")
+            decodeJSON(from: data)
         }else {
             print("Error \(data), HTTPS status \(response.statusCode) ")
         }
@@ -31,3 +32,60 @@ let task = URLSession.shared.dataTask(with: request) { data, res, error in
 }
 
 task.resume()
+
+
+
+func decodeJSON(from data: Data) {
+  let decoder = JSONDecoder()
+ 
+  if let movies = try? decoder.decode(MovieResponses.self, from: data) as MovieResponses {
+    print("PAGE: \(movies.page)")
+    print("TOTAL RESULTS: \(movies.totalResults)")
+    print("TOTAL PAGES: \(movies.totalPages)")
+      
+      print(movies.movies)
+      
+//      movies.movies.forEach { movie in
+//           print("TITLE: \(movie.title)")
+//           print("POSTER: \(movie.posterPath)")
+//           print("DATE: \(movie.releaseDate)")
+//         }
+  } else {
+    print("ERROR: Can't Decode JSON")
+  }
+}
+ 
+struct MovieResponses: Codable {
+  let page: Int
+  let totalResults: Int
+  let totalPages: Int
+  let movies: [MovieResponse]
+ 
+//  coding key for change key obj
+  enum CodingKeys: String, CodingKey {
+    case page
+    case totalResults = "total_results"
+    case totalPages = "total_pages"
+    case movies = "results"
+  }
+}
+
+struct MovieResponse: Codable {
+  let popularity: Double
+  let posterPath: String
+  let title: String
+  let genres: [Int]
+  let voteAverage: Double
+  let overview: String
+  let releaseDate: String
+ 
+  enum CodingKeys: String, CodingKey {
+    case popularity
+    case posterPath = "poster_path"
+    case title
+    case genres = "genre_ids"
+    case voteAverage = "vote_average"
+    case overview
+    case releaseDate = "release_date"
+  }
+}
